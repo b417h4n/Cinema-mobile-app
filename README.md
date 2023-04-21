63fb566a23a8f42bf9bfca4c29c0888f2c4a238b
Balzhan Urtakova <78582979+b417h4n@users.noreply.github.com>
2023-04-21 19:33:29 +0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Cinema-mobile-app
-Final project AITU students Cinema app allows get new popular movies  We get movies from The Movie Database API.  Open-source libraries used  Android Async HTTP - Simple asynchronous HTTP requests with JSON parsing Glide - Image loading and caching library for Android
+Final project AITU students 
+Cinema app allows get new popular movies 
+
+We get movies from [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+
+Open-source libraries used
+- [Android Async HTTP](https://github.com/codepath/CPAsyncHttpClient) - Simple asynchronous HTTP requests with JSON parsing
+- [Glide](https://github.com/bumptech/glide) - Image loading and caching library for Android
+
---
+++ app/src/main/java/com/example/FinalProjectCinema/models/Movie.java
@@ -0,0 +1,60 @@
+package com.example.FinalProjectCinema.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Movie {
+    String backdropPath;
+    String posterPath;
+    String title;
+    String overview;
+    double rating;
+    int movieId;
+
+    public Movie() {}
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+        backdropPath = jsonObject.getString("backdrop_path");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); ++i) {
+            Movie m = new Movie(movieJsonArray.getJSONObject(i));
+            movies.add(m);
+        }
+        return movies;
+    }
+
+    public String getPosterPath() {
+        //width of w342 is hardcoded for poster sizes
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", backdropPath);
+    }
+
+    public double getRating() { return rating; }
+
+    public int getMovieId() { return movieId; }
+}
